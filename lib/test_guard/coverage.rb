6b582bac693c79109bf07e093b6f15d0d4885eff
Chetan Sarva <chetan@pixelcop.net>
2013-09-16 16:41:40 -0400

    use randomized test commands for merging across runs

--- lib/test_guard/coverage.rb
+++ lib/test_guard/coverage.rb
@@ -4,6 +4,7 @@ module TestGuard
   def self.load_simplecov(&block)
     begin
       require 'simplecov'
+      SimpleCov.command_name "test:#{Time.new.strftime('%Y%m%d.%H%M%S')}"
       SimpleCov.start do
         add_filter "/test/"
         yield if block_given?
