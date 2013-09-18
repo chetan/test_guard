233c03ae1cfc62417ee9145d568c9dbe2deef20e
Chetan Sarva <chetan@pixelcop.net>
2013-09-18 15:54:04 -0400

    disabled turn & minitest by default

--- lib/test_guard.rb
+++ lib/test_guard.rb
@@ -2,7 +2,8 @@
 require "awesome_print"
 
 require "test_guard/coverage"
-require "test_guard/minitest"
 require "test_guard/rake"
-require "test_guard/turn"
 require "test_guard/debugger"
+
+# require "test_guard/minitest"
+# require "test_guard/turn"
