365b2ee9da9ee4c919e33979f73733d0e8f7453b
Chetan Sarva <chetan@pixelcop.net>
2013-11-20 11:45:54 -0500

    don't update instance var on each run

--- lib/test_guard/app/runner/micron.rb
+++ lib/test_guard/app/runner/micron.rb
@@ -7,13 +7,14 @@ class MicronRunner < Runner
   end
 
   def run(tests)
-    @cmd += @method_filter.map{ |m| "-m #{m}" } if @method_filter
-    @cmd += tests if tests
-    @cmd = @cmd.flatten.join(" ")
-    banner("running: #{@cmd}")
+    cmd = @cmd.dup
+    cmd += @method_filter.map{ |m| "-m #{m}" } if @method_filter
+    cmd += tests if tests
+    cmd = cmd.flatten.join(" ")
+    banner("running: #{cmd}")
 
     Dir.chdir(@path)
-    run_micron(@cmd)
+    run_micron(cmd)
   end
 
   def run_all
